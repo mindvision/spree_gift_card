a4f342f8c4b1afb0901875e202948301e67324e8
Michael Reitano <michael@mindvision.com.au>
2022-11-23 23:01:57 +1030

    54263: Handle adding and removing gift cards from checkout
    Change-Id: I07a2fa4de2fcd2ce84ab3d1a17f01c7022d9537b

---
+++ app/controllers/spree/api/v2/storefront/checkout_controller_decorator.rb
@@ -0,0 +1,61 @@
+# Extend Spree's checkout controller to handle adding and removing gift cards at checkout
+Spree::Api::V2::Storefront::CheckoutController.class_eval do
+  before_action :load_gift_card, :add_gift_card_payments, only: :update, if: :payment_via_gift_card?
+  before_action :remove_gift_card_payments, only: [:update]
+
+  private
+
+  ##
+  # Add gift card payments to order
+  # @return (Hash)
+  def add_gift_card_payments
+    order = spree_current_order
+    order.add_gift_card_payments(@gift_card)
+
+    render_serialized_payload(201) { serialize_resource(order.reload) }
+  end
+
+  ##
+  # Retrieve first available gift card payment method
+  # @return (Spree::PaymentMethod)
+  def gift_card_payment_method
+    @gift_card_payment_method ||= Spree::PaymentMethod.gift_card.available.first
+  end
+
+  ##
+  # Load gift card via code supplied in params
+  def load_gift_card
+    gift_card_attributes = params[:order][:payments_attributes].
+      find { |a| a[:payment_method_id] == gift_card_payment_method.id.to_s }
+    code = gift_card_attributes[:source_attributes][:code]
+    @gift_card = Spree::GiftCard.find_by(code: code)
+
+    render_error_payload(Spree.t(:gift_code_not_found)) unless @gift_card
+  end
+
+  ##
+  # Determine if paying with gift card by detecting if payment attributes include id for gift card
+  # payment method
+  # @return (Boolean)
+  def payment_via_gift_card?
+    if params[:order]
+      payment_attributes = params[:order].fetch(:payments_attributes, {}).present?
+      gift_card_method = params[:order][:payments_attributes]&.select do |payments_attribute|
+        gift_card_payment_method.try(:id).to_s == payments_attribute[:payment_method_id]
+      end.present?
+
+      payment_attributes && gift_card_method
+    end
+  end
+
+  ##
+  # Remove gift card payments from order
+  def remove_gift_card_payments
+    order = spree_current_order
+
+    if params.key?(:remove_gift_card) && order.using_gift_card?
+      order.remove_gift_card_payments
+      render_serialized_payload(201) { serialize_resource(order.reload) }
+    end
+  end
+end
