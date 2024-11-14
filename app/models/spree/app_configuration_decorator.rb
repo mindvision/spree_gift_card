a0dcf42a7ace7edf0822fa416b2e708e5c2768e3
Asim Sikka <asim@amroodlabs.com>
2024-11-14 19:40:45 +0500

    fixes necesssary for spree upgrade to 4.8

--- app/models/spree/app_configuration_decorator.rb
+++ app/models/spree/app_configuration_decorator.rb
@@ -1,3 +1,7 @@
 Spree::AppConfiguration.class_eval do
-  preference :allow_gift_card_redeem, :boolean, default: true
-end
+  if respond_to?(:preference)
+    preference :allow_gift_card_redeem, :boolean, default: true
+  else
+    Rails.logger.warn "Preference method is not defined on Spree::AppConfiguration"
+  end
+ end 
